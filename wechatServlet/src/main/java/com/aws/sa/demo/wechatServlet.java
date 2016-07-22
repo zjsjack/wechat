66d18dc217c67b6b95cf4f5ae58dc0e970fa215b
zhangjs <zhangjs@amazon.com>
2016-07-22 10:18:21 +0800

    bb

--- wechatServlet/src/main/java/com/aws/sa/demo/wechatServlet.java
+++ wechatServlet/src/main/java/com/aws/sa/demo/wechatServlet.java
@@ -1,9 +1,14 @@
 package com.aws.sa.demo;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,41 +23,81 @@ public class wechatServlet extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = -1508630456991652975L;
+	private String Token = "mk151020";
 
 	@Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");  
-        response.setCharacterEncoding("UTF-8");  
-  
-        /** 读取接收到的xml消息 */  
-        StringBuffer sb = new StringBuffer();  
-        InputStream is = request.getInputStream();  
-        InputStreamReader isr = new InputStreamReader(is, "UTF-8");  
-        BufferedReader br = new BufferedReader(isr);  
-        String s = "";  
-        while ((s = br.readLine()) != null) {  
-            sb.append(s);  
-        }  
-        String xml = sb.toString(); //次即为接收到微信端发送过来的xml数据  
-  
-        String result = "";  
-        /** 判断是否是微信接入激活验证，只有首次接入验证时才会收到echostr参数，此时需要把它直接返回 */  
-        String echostr = request.getParameter("echostr");  
-        if (echostr != null && echostr.length() > 1) {  
-            result = echostr;  
-        } else {  
-            //正常的微信处理流程  
-            result = new wechatProcess().processWechatMag(xml);  
-        }  
-  
-        try {  
-            OutputStream os = response.getOutputStream();  
-            os.write(result.getBytes("UTF-8"));  
-            os.flush();  
-            os.close();  
-        } catch (Exception e) {  
-            e.printStackTrace();  
-        }  
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
 
-   }
+		/** 读取接收到的xml消息 */
+		StringBuffer sb = new StringBuffer();
+		InputStream is = request.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+		String s = "";
+		while ((s = br.readLine()) != null) {
+			sb.append(s);
+		}
+		String xml = sb.toString(); // 接收到微信端发送过来的xml数据
+
+		String result = "";
+		/** 判断是否是微信接入激活验证，只有首次接入验证时才会收到echostr参数，此时需要把它直接返回 */
+		String echostr = request.getParameter("echostr");
+		if (echostr != null && echostr.length() > 1) {
+			String signature = request.getParameter("signature");
+			String timestamp = request.getParameter("timestamp");
+			String nonce = request.getParameter("nonce");
+			System.out.println(signature);
+			System.out.println(timestamp);
+			System.out.println(nonce);
+			System.out.println(echostr);
+			result = access(request, response);
+		} else {
+			// 正常的微信处理流程
+			result = new wechatProcess().processWechatMag(xml);
+		}
+
+		try {
+			OutputStream os = response.getOutputStream();
+			os.write(result.getBytes("UTF-8"));
+			os.flush();
+			os.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String access(HttpServletRequest request, HttpServletResponse response) {
+		// 验证URL真实性
+		System.out.println("进入验证access");
+		String signature = request.getParameter("signature");// 微信加密签名
+		String timestamp = request.getParameter("timestamp");// 时间戳
+		String nonce = request.getParameter("nonce");// 随机数
+		String echostr = request.getParameter("echostr");// 随机字符串
+		List<String> params = new ArrayList<String>();
+		params.add(Token);
+		params.add(timestamp);
+		params.add(nonce);
+		// 1. 将token、timestamp、nonce三个参数进行字典序排序
+		Collections.sort(params, new Comparator<String>() {
+			public int compare(String o1, String o2) {
+				return o1.compareTo(o2);
+			}
+		});
+		// 2. 将三个参数字符串拼接成一个字符串进行sha1加密
+		String temp = SHA1.encode(params.get(0) + params.get(1) + params.get(2));
+		if (temp.equals(signature)) {
+			try {
+				response.getWriter().write(echostr);
+				System.out.println("成功返回 echostr：" + echostr);
+				return echostr;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("失败 认证");
+		return null;
+	}
 }
